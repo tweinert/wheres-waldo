ed82faaf7ea9f55434643a5f572e67adfe416cdb
Tait Weinert <tait.weinert@outlook.com>
2022-07-21 18:11:05 +0930

    added click handler

--- src/index.js
+++ src/index.js
@@ -2,8 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getFirebaseConfig } from "./firebaseConfig";
 import WaldoImage from "./images/gobblingGluttons.jpg";
-import './style.css';
-
+import "./style.css";
 
 // initialize db
 const firebaseApp = initializeApp(getFirebaseConfig());
@@ -15,12 +14,15 @@ function createInitialDisplay() {
 
   const imgDiv = document.createElement("div");
   imgDiv.classList.add("imgDiv");
-  
+
+  // add click event handler
+  imgDiv.addEventListener("click", toggleTargetDisplay);
+
   const waldoImg = document.createElement("img");
   waldoImg.setAttribute("src", WaldoImage);
-  
+
   imgDiv.appendChild(waldoImg);
-  
+
   content.appendChild(imgDiv);
 }
 
@@ -30,7 +32,6 @@ function createTargetDisplay() {
   const targetDiv = document.createElement("div");
   targetDiv.id = "targetDiv";
   // TODO hide display initially
-  targetDiv.setAttribute("position", "absolute");
 
   const targetBox = document.createElement("div");
   targetBox.id = "targetBox";
@@ -53,13 +54,16 @@ function createTargetDisplay() {
   content.appendChild(targetDiv);
 }
 
-function toggleTargetDisplay() {
-  // set display: absolute on click.
-  // set position to click position
-  // set display none after timer ends
+function toggleTargetDisplay(e) {
+  // coords of click
+  xPos = e.clientX;
+  yPos = e.clientY;
 
+  const targetDisplay = document.getElementById("targetDiv");
+  targetDisplay.parentNode.appendChild(targetDisplay);
+  targetDisplay.style.left = xPos - 50 + "px";
+  targetDisplay.style.top = yPos - 50 + "px";
 }
 
 createInitialDisplay();
 createTargetDisplay();
-
