1fc4768fb2528f4a64eddd6996ca0c7b25c6150e
Tait Weinert <tait.weinert@outlook.com>
2022-07-24 12:04:58 +0930

    added target display timer

--- src/mouseFunctions.js
+++ src/mouseFunctions.js
@@ -1,4 +1,4 @@
-export function clickTargetDisplay(e) {
+export async function clickTargetDisplay(e) {
   // coords of click
   let xPos = e.clientX;
   let yPos = e.clientY;
@@ -7,4 +7,9 @@ export function clickTargetDisplay(e) {
   targetDisplay.parentNode.appendChild(targetDisplay);
   targetDisplay.style.left = xPos - 50 + "px";
   targetDisplay.style.top = yPos - 50 + "px";
+
+  setTimeout(() => {
+    targetDisplay.style.left = "5000px";
+    console.log("timer started");
+  }, 3000);
 }
